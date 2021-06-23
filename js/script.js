c53d4dba7faefc0d598c0a17ea22a58f853ccc87
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-06-23 15:12:39 +0200

    add vue script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 // - stampare in pagina un item per ogni elemento contenuto in un array
 // - ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
 // - predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
+
+const app = new Vue(
+    {
+        el : '#app',
+
+        data : {
+            newProm : '',
+            arrayPromemoria : ['giocare a pallone','fare i compiti','studiare matematica']
+        },
+        methods : {
+            inserisci(){
+                if (this.newProm) {
+                    this.arrayPromemoria.push(this.newProm);
+                    this.newProm = '';
+                }
+            },
+            cancella(indice){
+                this.arrayPromemoria.splice(indice,1);
+            }
+        }
+    }
+    );
